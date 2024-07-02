b04fa0c10290a55968dd72807e782fa059d460c8
Fu Yaohui <diligencefu@sina.com>
2024-07-02 15:00:12 +0800

    适配

--- LNTools_fyh/LNViewExtension/Classes/Toast.swift
+++ LNTools_fyh/LNViewExtension/Classes/Toast.swift
@@ -5,7 +5,7 @@
 //  Created by MAC on 2020/10/11.
 //
 
-import Toast
+//import Toast
 import UIKit
 
 @objc public extension UIView {
@@ -19,26 +19,26 @@ import UIKit
     
     func ln_showToast(str:String,position:WWZLToastPosition = WWZLToastPosition.center,isUserInterfaceEnableWhenShow:Bool = true) {
         
-        var tPsition = String()
-        switch position {
-        case .top:
-            tPsition = CSToastPositionTop
-        case .center:
-            tPsition = CSToastPositionCenter
-        default:
-            tPsition = CSToastPositionBottom
-        }
-        DispatchQueue.main.async {
-            let kWindow = UIApplication.shared.keyWindow
-            if !isUserInterfaceEnableWhenShow {
-                kWindow?.isUserInteractionEnabled = false
-            }
-            let style = CSToastStyle.init(defaultStyle: ())
-            kWindow?.makeToast(str, duration: 2.0, position: tPsition, style: style)
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1.5, execute: {
-                kWindow?.isUserInteractionEnabled = true
-            })
-        }
+//        var tPsition = String()
+//        switch position {
+//        case .top:
+//            tPsition = CSToastPositionTop
+//        case .center:
+//            tPsition = CSToastPositionCenter
+//        default:
+//            tPsition = CSToastPositionBottom
+//        }
+//        DispatchQueue.main.async {
+//            let kWindow = UIApplication.shared.keyWindow
+//            if !isUserInterfaceEnableWhenShow {
+//                kWindow?.isUserInteractionEnabled = false
+//            }
+//            let style = CSToastStyle.init(defaultStyle: ())
+//            kWindow?.makeToast(str, duration: 2.0, position: tPsition, style: style)
+//            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1.5, execute: {
+//                kWindow?.isUserInteractionEnabled = true
+//            })
+//        }
     }
 }
 
